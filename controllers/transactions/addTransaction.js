dd1e942f0456f4238981df1a85ce47b1c7f37ca4
Vladimir Bondar <vladi.bondar@gmail.com>
2022-04-06 02:27:24 +0200

    add balance

--- controllers/transactions/addTransaction.js
+++ controllers/transactions/addTransaction.js
@@ -1,18 +1,20 @@
-const { Transaction } = require('../../models');
+const { Transaction, User } = require('../../models');
 
 const addTransaction = async (req, res) => {
   const { _id } = req.user;
-
-  const data = await Transaction.find({ owner: _id });
-  const sortedData = [...data].sort((a, b) => b.updatedAt - a.updatedAt);
-
   const { amount, type } = req.body;
-  if (data.length === 0) {
-    sortedData.push({ balance: 0 });
-  }
-  const balance = type ? sortedData[0].balance + amount : sortedData[0].balance - amount;
 
-  res.status(201).json(await Transaction.create({ ...req.body, owner: _id, balance }));
+  const user = await User.findById(_id);
+
+  user.setBalance(type ? user.balance + amount : user.balance - amount).save();
+
+  res.status(201).json(
+    await Transaction.create({
+      ...req.body,
+      owner: _id,
+      balance: user.balance,
+    }),
+  );
 };
 
 module.exports = addTransaction;
