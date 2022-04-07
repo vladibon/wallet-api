4edc0507f20efa7a1668ec825ee5f114e7ea2744
Michael Paukov <michael.paukov@gmail.com>
2022-04-07 11:43:47 +0200

    fix balance (negativ, round)

--- controllers/transactions/addTransaction.js
+++ controllers/transactions/addTransaction.js
@@ -6,21 +6,28 @@ const addTransaction = async (req, res) => {
 
   const user = await User.findById(_id);
 
-  user.setBalance(type ? user.balance + amount : user.balance - amount).save();
+  const balance = type ? user.balance + amount : user.balance - amount;
 
-  await Transaction.create({
-    ...req.body,
-    owner: _id,
-    balance: user.balance,
-  });
+  if (balance < 0) {
+    res.status(400).json({ message: 'Balance cannot be below zero' });
+  } else {
+    user.setBalance(Math.round(balance * 100) / 100).save();
+    await Transaction.create({
+      ...req.body,
+      owner: _id,
+      balance: user.balance,
+    });
 
-  const skip = 0;
-  const limit = 8;
-  const transactions = await Transaction.find({ owner: _id }, { owner: 0 }, { skip, limit }).sort({
-    createdAt: -1,
-  });
-  const result = { transactions, balance: user.balance, page: 1 };
-  res.status(201).json(result);
+    const skip = 0;
+    const limit = 8;
+    const transactions = await Transaction.find({ owner: _id }, { owner: 0 }, { skip, limit }).sort(
+      {
+        createdAt: -1,
+      },
+    );
+    const result = { transactions, balance: user.balance, page: 1 };
+    res.status(201).json(result);
+  }
 };
 
 module.exports = addTransaction;
