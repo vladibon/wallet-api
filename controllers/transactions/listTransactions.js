ff8ffaed3f462215f0aff919485d453f3c755a9e
Vladimir Bondar <vladi.bondar@gmail.com>
2022-04-09 15:07:54 +0200

    fix type of page to number

--- controllers/transactions/listTransactions.js
+++ controllers/transactions/listTransactions.js
@@ -12,9 +12,10 @@ const listTransactions = async (req, res) => {
   const transactions = await Transaction.find({ owner: _id }, { owner: 0 }, { skip, limit }).sort({
     createdAt: -1,
   });
+
   const totalPages = Math.ceil(totalTransactions / limit);
 
-  res.json({ transactions, balance, page, totalPages });
+  res.json({ transactions, balance, page: Number(page), totalPages });
 };
 
 module.exports = listTransactions;
