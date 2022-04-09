96f997dbc3da21632e31adfae8d4502475694fd7
Vladimir Bondar <vladi.bondar@gmail.com>
2022-04-09 22:11:14 +0200

    fix statistic endpoint

--- controllers/transactions/statsTransaction.js
+++ controllers/transactions/statsTransaction.js
@@ -2,31 +2,56 @@ const { Transaction } = require('../../models');
 
 const statsTransaction = async (req, res) => {
   const { _id } = req.user;
-  const { begin, end, type } = req.body;
+  const { month, year } = req.body;
 
-  const result = await Transaction.aggregate([
+  const income = await Transaction.aggregate([
     {
       $match: {
         owner: _id,
-        type: type,
+        type: true,
         date: {
-          $gte: new Date(begin), // тут указываете с какого числа месяца
-          $lte: new Date(end), // по какое число месяца.
+          $gte: new Date(`${month}/1/${year}`),
+          $lte: new Date(`${month}/31/${year}`),
         },
       },
     },
     {
       $group: {
         _id: { category: '$category' },
-        totalSum: { $sum: '$amount' },
+        sum: { $sum: '$amount' },
       },
     },
     {
-      $project: { _id: 0, category: '$_id.category', totalSum: '$totalSum' },
+      $project: { _id: 0, category: '$_id.category', sum: '$sum' },
     },
   ]);
 
-  res.json(result);
+  const expense = await Transaction.aggregate([
+    {
+      $match: {
+        owner: _id,
+        type: false,
+        date: {
+          $gte: new Date(`${month}/1/${year}`),
+          $lte: new Date(`${month}/31/${year}`),
+        },
+      },
+    },
+    {
+      $group: {
+        _id: { category: '$category' },
+        sum: { $sum: '$amount' },
+      },
+    },
+    {
+      $project: { _id: 0, category: '$_id.category', sum: '$sum' },
+    },
+  ]);
+
+  const totalIncome = income.reduce((acc, { sum }) => acc + sum, 0);
+  const totalExpenses = expense.reduce((acc, { sum }) => acc + sum, 0);
+
+  res.json({ income, expense, totalIncome, totalExpenses });
 };
 
 module.exports = statsTransaction;
