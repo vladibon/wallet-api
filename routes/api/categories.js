81a57d3d79ed319c08daecb8b47927ef171621cf
Vladimir Bondar <vladi.bondar@gmail.com>
2022-04-13 20:31:45 +0200

    clear extra code

--- routes/api/categories.js
+++ routes/api/categories.js
@@ -1,9 +1,5 @@
 const express = require('express');
-
-// const { auth, validation, ctrlWrapper } = require('../../middlewares');
 const { ctrlWrapper } = require('../../middlewares');
-// const { joiSchema, emailJoiSchema } = require('../../models/user');
-// const { signupJoiSchema, loginJoiSchema } = require('../../models/user');
 const { categories: ctrl } = require('../../controllers');
 
 const router = express.Router();
