5f0c73c4cbab09018e6a821bd7789eee5520dc7e
Olena-Horobets <80359219+Olena-Horobets@users.noreply.github.com>
2022-04-21 23:41:20 +0300

    update for running test mode

--- controllers/users/deleteUser.js
+++ controllers/users/deleteUser.js
@@ -1,9 +1,13 @@
-const removeOldAvatars = require('../../services/removeOldAvatars');
+const { Forbidden } = require('http-errors');
 
+const removeOldAvatars = require('../../services/removeOldAvatars');
+const { TEST_USER_ID } = process.env;
 const { Transaction, User } = require('../../models');
 
 const deleteUser = async (req, res) => {
   const { _id } = req.user;
+
+  if (_id.toString() === TEST_USER_ID) throw new Forbidden('Sorry, you cannot delete test user');
 
   await Transaction.deleteMany({ owner: _id });
   await User.deleteOne({ id: _id });
--- controllers/users/updateEmail.js
+++ controllers/users/updateEmail.js
@@ -1,8 +1,14 @@
+const { Forbidden } = require('http-errors');
+
+const { TEST_USER_ID } = process.env;
 const { User } = require('../../models');
 
 const updateEmail = async (req, res) => {
   const { _id } = req.user;
   const { email } = req.body;
+
+  if (_id.toString() === TEST_USER_ID)
+    throw new Forbidden('Sorry, you cannot change email for test user');
 
   const updatedUser = await User.findByIdAndUpdate(_id, { email }, { new: true });
 
