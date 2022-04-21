f6ace72653381f51263b85fd5e7dda7acda5798a
Vladimir Bondar <vladi.bondar@gmail.com>
2022-04-22 01:34:48 +0200

    fix

--- middlewares/checkTestUser.js
+++ middlewares/checkTestUser.js
@@ -4,7 +4,8 @@ const { TEST_USER_ID } = process.env;
 const checkTestUser = async (req, _, next) => {
   const { _id } = req.user;
 
-  if (_id === TEST_USER_ID) throw new Forbidden('Sorry, you are not allowed to make these changes');
+  if (String(_id) === TEST_USER_ID)
+    throw new Forbidden('Sorry, you are not allowed to make these changes');
 
   next();
 };
