7d4927cd5ad86889b9e56fb78a8361a42ab42702
Vladimir Bondar <vladi.bondar@gmail.com>
2022-04-24 15:07:47 +0200

    fix deleteOne

--- controllers/users/deleteUser.js
+++ controllers/users/deleteUser.js
@@ -5,7 +5,7 @@ const deleteUser = async (req, res) => {
   const { _id } = req.user;
 
   await Transaction.deleteMany({ owner: _id });
-  await User.deleteOne({ id: _id });
+  await User.deleteOne({ _id });
   await removeOldAvatars(_id);
 
   res.status(200).json({ message: 'User has been deleted' });
